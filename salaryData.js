bfafe230a71ccc1b99745edfc3599464f2849e52
asraful196 <ahamedsunny14@gmail.com>
2021-04-03 00:39:46 +0600

    salary app

---
+++ salaryData.js
@@ -0,0 +1,31 @@
+var salary_data = {
+  "0" : {
+    "hobby" : "Cricket",
+    "name" : "Miraz Ahamed",
+    "salary" : "200000"
+  },
+  "1" : {
+    "name" : "Sakibul hasan",
+    "salary" : "94320.0",
+    "status" : "married"
+  },
+  "2" : {
+    "name" : "Arfin Misu",
+    "salary" : "67000.0"
+  },
+  "3" : {
+    "name" : "Fahim Ahamed",
+    "salary" : "30554.0"
+  },
+  "4" : {
+    "name" : "Mainul Hudah",
+    "salary" : "82220.0"
+  },
+ 
+  "5" : {
+    "name" : "Sunny Ashraf",
+    "salary" : "30000"
+    
+  }
+
+}
